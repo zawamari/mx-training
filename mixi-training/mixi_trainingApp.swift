5181284ca38c5620024880dfc543f6c07f0d76ae
Marie <marie@TT-no-MacBook-Air-3.local>
2024-05-27 11:41:03 +0900

    first commit

---
+++ mixi-training/mixi_trainingApp.swift
@@ -0,0 +1,17 @@
+//
+//  mixi_trainingApp.swift
+//  mixi-training
+//
+//  Created by Marie on 2024/05/27.
+//
+
+import SwiftUI
+
+@main
+struct mixi_trainingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
